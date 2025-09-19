f272393dbe17da14dc32b6878fa8f8b216ab09de
Kratik jain <130534361+Its-Kratik@users.noreply.github.com>
2025-09-19 14:50:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import torch
 
 @st.cache_resource(show_spinner=True)
 def load_model():
-    model_name = "ai4bharat/indictrans2-indic-indic-1B@24d7329"
+    model_name = "ai4bharat/indictrans2-indic-indic-1B"
     tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name, trust_remote_code=True)
     model = model.to("cpu")
